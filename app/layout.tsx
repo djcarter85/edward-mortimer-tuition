a303dc89aa7539902daad20c4c2cbf383c96a65b
Dan Carter <djcarter85@gmail.com>
2026-01-24 20:11:18 +0000

    chore: install Tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 
+import './globals.css'
+
 export const metadata: Metadata = {
   title: 'Next.js on GitHub Pages',
   description: 'Deploy your static Next.js site to GitHub Pages.',
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <div>Next.js on GitHub Pages</div>
-      <div>Template site for Edward Mortimer Tuition</div>
+      <div className="font-bold text-blue-500">Template site for Edward Mortimer Tuition</div>
     </main>
   );
 }
