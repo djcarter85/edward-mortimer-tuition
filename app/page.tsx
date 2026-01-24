2cb3baf3face528747672d11c795fe065fa9a936
Dan Carter <djcarter85@gmail.com>
2026-01-24 21:41:23 +0000

    feat: add Contact section

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { GraduationCap } from "lucide-react";
 
 const Hero = () => {
   return (
-    <section className="flex flex-col gap-4 p-10">
+    <section className="flex w-full flex-col gap-4 p-10">
       <div className="text-center text-5xl font-bold">
         Edward Mortimer Tuition
       </div>
@@ -15,7 +15,7 @@ const Hero = () => {
 
 const ServiceListItem = ({ text }: { text: string }) => {
   return (
-    <li className="flex flex-row gap-2 items-center my-1">
+    <li className="my-1 flex flex-row items-center gap-2">
       <GraduationCap className="size-5" /> <span>{text}</span>
     </li>
   );
@@ -23,7 +23,7 @@ const ServiceListItem = ({ text }: { text: string }) => {
 
 const Services = () => {
   return (
-    <section className="flex flex-col gap-2 mx-auto max-w-lg">
+    <section className="mx-auto flex w-full max-w-lg flex-col gap-2">
       <div className="text-lg font-bold">
         Would your child benefit from high-quality English tuition?
       </div>
@@ -37,11 +37,24 @@ const Services = () => {
   );
 };
 
+const Contact = () => {
+  return (
+    <section className="mx-auto flex w-full max-w-lg flex-col gap-2">
+      <div className="text-lg font-bold">Message me for more information</div>
+      <div>
+        <div>SMS: 07775 213822</div>
+        <div>Email: edwardmortimer@live.com</div>
+      </div>
+    </section>
+  );
+};
+
 export default function Home() {
   return (
-    <main className="mx-auto min-h-screen max-w-4xl px-4">
+    <main className="mx-auto flex min-h-screen max-w-4xl flex-col gap-4 px-4">
       <Hero />
       <Services />
+      <Contact />
     </main>
   );
 }
