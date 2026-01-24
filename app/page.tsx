dd52b71fe130b0e3ffdab65485d915a96fa11851
Dan Carter <djcarter85@gmail.com>
2026-01-24 20:39:54 +0000

    feat: add some styling and an image

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Image from "next/image";
+import HeroImage from './hero.jpg'
+
 export default function Home() {
   return (
-    <main>
+    <main className="mx-auto min-h-screen max-w-2xl bg-amber-100">
+      <Image src={HeroImage} alt="" />
       <div>Next.js on GitHub Pages</div>
       <div className="font-bold text-blue-500">
         Template site for Edward Mortimer Tuition
