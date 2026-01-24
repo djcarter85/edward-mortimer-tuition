e052c519f71b55cede3d706f5f7fe2787dcc3e2d
Dan Carter <djcarter85@gmail.com>
2026-01-24 20:21:10 +0000

    chore: apply Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
-import './globals.css'
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Next.js on GitHub Pages',
-  description: 'Deploy your static Next.js site to GitHub Pages.',
+  title: "Next.js on GitHub Pages",
+  description: "Deploy your static Next.js site to GitHub Pages.",
 };
 
 export default function RootLayout({
