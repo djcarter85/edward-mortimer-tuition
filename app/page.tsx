218599a3e628a09d55c74d4b5d59a5ea976063e4
Dan Carter <djcarter85@gmail.com>
2026-01-24 21:23:32 +0000

    feat: add basic hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
+const Hero = () => {
+  return (
+    <section className="flex flex-col gap-4 p-10">
+      <div className="text-center text-5xl font-bold">
+        Edward Mortimer Tuition
+      </div>
+      <div className="text-center text-xl">
+        Expert English tuition in your home
+      </div>
+    </section>
+  );
+};
+
 export default function Home() {
   return (
-    <main className="mx-auto min-h-screen max-w-2xl bg-amber-100">
-      <div>Next.js on GitHub Pages</div>
-      <div className="font-bold text-blue-500">
-        Template site for Edward Mortimer Tuition
-      </div>
+    <main className="mx-auto min-h-screen max-w-4xl">
+      <Hero />
     </main>
   );
 }
