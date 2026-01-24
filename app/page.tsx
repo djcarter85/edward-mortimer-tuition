f4ccb7cb2449abe08d20e70f9f28ee97c4f69e19
Dan Carter <djcarter85@gmail.com>
2026-01-24 21:35:37 +0000

    feat: add services section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { GraduationCap } from "lucide-react";
+
 const Hero = () => {
   return (
     <section className="flex flex-col gap-4 p-10">
@@ -11,10 +13,35 @@ const Hero = () => {
   );
 };
 
+const ServiceListItem = ({ text }: { text: string }) => {
+  return (
+    <li className="flex flex-row gap-2 items-center my-1">
+      <GraduationCap className="size-5" /> <span>{text}</span>
+    </li>
+  );
+};
+
+const Services = () => {
+  return (
+    <section className="flex flex-col gap-2 mx-auto max-w-lg">
+      <div className="text-lg font-bold">
+        Would your child benefit from high-quality English tuition?
+      </div>
+      <ul>
+        <ServiceListItem text="Exam preparation" />
+        <ServiceListItem text="Spelling, punctuation and grammar" />
+        <ServiceListItem text="Reading comprehension" />
+        <ServiceListItem text="Homework assignments" />
+      </ul>
+    </section>
+  );
+};
+
 export default function Home() {
   return (
-    <main className="mx-auto min-h-screen max-w-4xl">
+    <main className="mx-auto min-h-screen max-w-4xl px-4">
       <Hero />
+      <Services />
     </main>
   );
 }
